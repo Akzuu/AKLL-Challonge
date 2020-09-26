b535c4d642c120877ad0d60e982f4aec21d1b9ed
Akseli Kolari <akseli.kolari@tuni.fi>
2020-09-26 18:25:04 +0300

    Docker stuff

---
+++ config/custom-environment-variables.js
@@ -0,0 +1,12 @@
+module.exports = {
+  host: 'HOST',
+  database: {
+    mongo: {
+      uri: 'MONGODB_URI',
+    },
+  },
+  challonge: {
+    apiKey: 'CHALLONGE_API_KEY',
+  },
+  coreBackendUrl: 'AKLL_BACKEND_URL',
+};
